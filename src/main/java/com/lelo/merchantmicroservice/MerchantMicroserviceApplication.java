0c83d03f972b06e4bab6b7ebf3a3e148d1e896cc
dhruvacharya <dhruv.acharya007@gmail.com>
2019-01-19 18:13:53 +0530

    Initial commit

---
+++ src/main/java/com/lelo/merchantmicroservice/MerchantMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.lelo.merchantmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantMicroserviceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/lelo/merchantmicroservice/MerchantMicroserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lelo.merchantmicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MerchantMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
